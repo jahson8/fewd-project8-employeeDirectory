904c97515a3afdd9f47a32269305b50e49f193cb
J Mill <jahson8@hotmail.com>
2021-05-05 13:54:55 -0500

    update modal functionality

--- js/app.js
+++ js/app.js
@@ -4,34 +4,32 @@ email, location, phone, dob &noinfo &nat=US`;
 const directory = document.getElementById("directory");
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal");
-const modalContent = document.querySelector(".modal-content")
-
+const modalContent = document.querySelector(".modal-content");
 
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
-const fetchData = url =>{
-    return fetch(url)
-            .then(checkStatus)
-            .then(response => response.json())
-            .catch(err => console.log("Looks like there was a problem receiving  api data.", err))
+const fetchData = (url) => {
+  return fetch(url)
+    .then(checkStatus)
+    .then((response) => response.json())
+    .catch((err) =>
+      console.log("Looks like there was a problem receiving  api data.", err)
+    );
+};
+
+//Checks the
+function checkStatus(res) {
+  if (res.ok) {
+    return Promise.resolve(res);
+  } else {
+    generateErrorMsg();
+    return Promise.reject(new Error(response.statusText));
+  }
 }
 
-//Checks the 
-function checkStatus(res){
-    if(res.ok){
-      return Promise.resolve(res)
-    }else {
-        generateErrorMsg();
-      return Promise.reject(new Error(response.statusText))
-    }
-  }
-
-
 //Api call
-fetchData(urlAPI)
-.then(data => generateEmployeeHTML(data.results))
-
+fetchData(urlAPI).then((data) => generateEmployeeHTML(data.results));
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
@@ -39,14 +37,14 @@ fetchData(urlAPI)
 
 //Create the employee list seen on page on load
 const generateEmployeeHTML = (data) => {
-    employees = data;
-    let html = "";
-    employees.forEach((employee, index) => {
-        let name = employee.name;
-        let email = employee.email;
-        let city = employee.location.city;
-        let picture = employee.picture;
-        html += `<li class="employee" data-index=${index}>
+  employees = data;
+  let html = "";
+  employees.forEach((employee, index) => {
+    let name = employee.name;
+    let email = employee.email;
+    let city = employee.location.city;
+    let picture = employee.picture;
+    html += `<li class="employee" data-index=${index}>
                                     <a href="#">
                                         <img class="img-responsive" src="${picture.large}" alt="Start up employee ${name.first} ${name.last}">
                                     <div class="caption">
@@ -55,23 +53,31 @@ const generateEmployeeHTML = (data) => {
                                         <p>${city}</p>
                                     </div>
                                 </a>
-                </li>`  ;   
-    });
-    directory.innerHTML = html;
-}
-
+                </li>`;
+  });
+  directory.innerHTML = html;
+};
 
 //Generate modal
-const generateModal = (index) =>{
-    let { name, dob, phone, email, location: { city, street, state, postcode
-    }, picture } = employees[index];
+const generateModal = (index) => {
+  let {
+    name,
+    dob,
+    phone,
+    email,
+    location: { city, street, state, postcode },
+    picture,
+  } = employees[index];
 
-    // Format birthdate
-    let date = new Date(dob.date);
-    let birthday =  ('0' + date.getMonth()).slice(-2) + '/'
-    + ('0' + (date.getDay()+1)).slice(-2) + '/'
-    + date.getYear();
-    let html = `
+  // Format birthdate
+  let date = new Date(dob.date);
+  let birthday =
+    ("0" + date.getMonth()).slice(-2) +
+    "/" +
+    ("0" + (date.getDay() + 1)).slice(-2) +
+    "/" +
+    date.getYear();
+  let html = `
             <div class="modal-wrap" data-index=${index}>
                 <img class="img-responsive" src="${picture.large}" alt="${name.first} ${name.last}">
                 <div class="name">
@@ -85,101 +91,75 @@ const generateModal = (index) =>{
                     <P>Birthday: ${birthday}</P>
                 </div>
             </div>`;
-    overlay.classList.remove("hidden");
-    modalContent.innerHTML = html;
-}
-
-
+  overlay.classList.remove("hidden");
+  modalContent.innerHTML = html;
+};
 
 //Create error message html seen if the api data fails to load
 const generateErrorMsg = () => {
-    const employeesSection = document.getElementById("employees");
-    const msg = `
+  const employeesSection = document.getElementById("employees");
+  const msg = `
         <div class="error-msg">
             <h2>Error</h2>
             <p>There was a problem receiving employee data. Please try again later.</p>
         </div>
     `;
-    employeesSection.innerHTML = msg;
-
-}
-
-
+  employeesSection.innerHTML = msg;
+};
 
 // ------------------------------------------
 //  Event Handlers
 // ------------------------------------------
 
-// const handleModalClose = evt => {
-//     const close = evt.target;
-//     if(close.tagName === "BUTTON"){
-//         if(close.classList.contains("close")){
-//             overlay.classList.add("hidden"); 
-  
-//         }
-//     }
-    
-// }
-
-
 //creates new model
-const modalChange = num =>{
-    let index = modalContent.querySelector(".modal-wrap").getAttribute("data-index");
-    index = parseInt(index);
-    if(index >= 0 || index <= employees.length){
-        index = index + num;
-        generateModal(index);
-    } else {
-        modalContent.removeChild(btn)
-    }
-
-} 
-
-
+const modalChange = (num) => {
+  let index = modalContent
+    .querySelector(".modal-wrap")
+    .getAttribute("data-index");
+  let employees = document.querySelectorAll(".employee");
+  index = parseInt(index);
+  index = index + num;
+  if (index < 0) {
+    index = employees.length - 1;
+  } else if (index > employees.length - 1) {
+    index = 0;
+  }
+  generateModal(index);
+};
 // Handles the closing of the modal
-const handleModalClose= evt=>{
-    const btn = evt.target;
-    if(btn.tagName === "BUTTON"){
-        if(btn.classList.contains("close")){
-            overlay.classList.add("hidden");
-        } 
+const handleModalClose = (evt) => {
+  const btn = evt.target;
+  if (btn.tagName === "BUTTON") {
+    if (btn.classList.contains("close")) {
+      overlay.classList.add("hidden");
     }
-}
-
+  }
+};
 
 // Handles the changing of the modal content
-const handleModalChange = evt => {
-    let btn = evt.target;
-    if(btn.tagName === "BUTTON"){
-        if(btn.classList.contains("right")){
-            modalChange(1);
-        } else if(btn.classList.contains("left")){
-            modalChange(-1);
-        }
+const handleModalChange = (evt) => {
+  let btn = evt.target;
+  if (btn.tagName === "BUTTON") {
+    if (btn.classList.contains("right")) {
+      modalChange(1);
+    } else if (btn.classList.contains("left")) {
+      modalChange(-1);
     }
-
-}
-
-
+  }
+};
 
 // ------------------------------------------
 //  Event Listeners
 // ------------------------------------------
-directory.addEventListener('click', evt => {
-
-// make sure the click is not on the gridContainer itself
-    if (evt.target !== directory) {
+directory.addEventListener("click", (evt) => {
+  // make sure the click is not on the gridContainer itself
+  if (evt.target !== directory) {
     // select the card element based on its proximity to actual element clicked
     const employee = evt.target.closest(".employee");
-    const index = employee.getAttribute('data-index');
+    const index = employee.getAttribute("data-index");
     generateModal(index);
-    }
+  }
 });
 
 modalContainer.addEventListener("click", handleModalClose);
 modalContainer.addEventListener("click", handleModalChange);
-
-
-
-
-
